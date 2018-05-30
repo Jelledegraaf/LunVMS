b6625465dfc0be5a42797e4df784cf04886c484b
ismailnegro <39089588+ismailnegro@users.noreply.github.com>
2018-05-30 11:01:02 +0200

    ?

--- src/AppBundle/Controller/BestelregelController.php
+++ src/AppBundle/Controller/BestelregelController.php
@@ -30,6 +30,6 @@ class BestelregelController extends Controller
             return $this->redirect($this->generateurl("bestelregelnieuw"));
         }
 
-        return new Response($this->renderView('form.html.twig', array('form' => $form->createView())));
+        return new Response($this->render('form.html.twig', array('form' => $form->createView())));
     }
 }
